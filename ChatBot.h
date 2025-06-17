6bcb3d89370d4ba63bd01f0b88e12a5bc7611fa4
rian-fuzinelli <riancaiofuzinelli12@gmail.com>
2025-06-16 23:54:45 -0300

    feat: adding some variables in the main page

--- ChatBot.h
+++ ChatBot.h
@@ -1,9 +1,21 @@
 #ifndef CHATBOT_H
 #define CHATBOT_H
+#include <map>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 class ChatBot {
+private:
+    map<string, string> answers;
+    vector<string> history;
+    string nameBot;
+
+    string upperMessage(string text);
+    string cleanInput(string input);
+    void addToHistory(const string& user, string& message);
+    void initializeAnswers();
 
 };
 
